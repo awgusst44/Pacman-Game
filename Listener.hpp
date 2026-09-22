4c435e0e407ea542ae7cc11caf05ca33bd0886f4
awgusst44 <moisei.iulia@usm.md>
2026-09-23 00:31:51 +0300

    Update Listener.hpp

--- Listener.hpp
+++ Listener.hpp
@@ -4,23 +4,16 @@
 
 namespace pacman {
 
-// Captures raw user input (keyboard) and translates it into game commands.
-// The concrete input backend (console, SFML, SDL, etc.) can be plugged in
-// by implementing these methods in a .cpp file.
 class Listener {
 public:
     Listener();
 
-    // Polls the input backend for new events. Should be called once per frame.
     void poll();
 
-    // Returns the most recently requested movement direction.
     Direction getRequestedDirection() const { return requestedDirection_; }
 
-    // Returns true if the user requested to pause/resume the game this frame.
     bool wasPauseRequested() const { return pauseRequested_; }
 
-    // Returns true if the user requested to quit the game.
     bool wasQuitRequested() const { return quitRequested_; }
 
 private:
@@ -29,4 +22,4 @@ private:
     bool quitRequested_ = false;
 };
 
-} // namespace pacman
+} 
